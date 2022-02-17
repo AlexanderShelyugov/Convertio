4a436ea4a8a5464714f9646f1c5054ff77117c87
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2022-02-17 20:43:34 +0300

    Init Spring Boot project using start.spring.io

---
+++ src/main/java/ru/alexander/testtask/ConcurrencyConvertorApplication.java
@@ -0,0 +1,13 @@
+package ru.alexander.testtask.currencyconvertor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyConvertorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyConvertorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/alexander/testtask/ConcurrencyConvertorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alexander.testtask.currencyconvertor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyConvertorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
