0850ba632eb52f2d223b4377303fd4ddc5520870
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2022-02-17 22:11:01 +0300

    Restructure project

---
+++ convertio-web-api/src/main/java/ru/alexander/convertio/web/api/ConvertionsController.java
@@ -0,0 +1,7 @@
+package ru.alexander.convertio.web.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class ConvertionsController {
+}
---
+++ convertio-web-api/src/test/java/ru/alexander/convertio/web/api/ConvertionsControllerTests.java
@@ -0,0 +1,7 @@
+package ru.alexander.convertio.web.api;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(ConvertionsController.class)
+class ConvertionsControllerTests {
+}
