b2dfdf4bcb65ca242c1b6b50904f57f759adc3ea
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2022-02-20 22:16:21 +0300

    Add description for currencies service

--- convertio-web-api/src/main/java/ru/alexander/convertio/web/api/CurrenciesController.java
+++ convertio-web-api/src/main/java/ru/alexander/convertio/web/api/CurrenciesController.java
@@ -1,5 +1,10 @@
 package ru.alexander.convertio.web.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.alexander.convertio.conversions.api.CurrenciesService;
+
+import java.util.Map;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.ResponseEntity.ok;
@@ -17,6 +24,15 @@ import static org.springframework.http.ResponseEntity.ok;
 class CurrenciesController {
     private final CurrenciesService currenciesService;
 
+    @Operation(summary = "Get all supported currencies and their descriptions")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200",
+            description = "Retrieval successful",
+            content = {
+                @Content(mediaType = APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = Map.class))
+            })
+    })
     @GetMapping
     ResponseEntity<?> getSupportedCurrencies() {
         val currencies = currenciesService.getAllSupportedCurrencies();
